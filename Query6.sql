028e249471500ce8370f189d9dee9b1f33c19c0a
Zachary <zacharyh728@gmail.com>
2024-02-15 00:46:15 -0800

    Wrote and Ran Query6.sql

---
+++ Query6.sql
@@ -0,0 +1,7 @@
+SELECT album.Title AS albums, album.ArtistId, SUM(invoice_item.Quantity) AS sold
+FROM albums album
+JOIN tracks track ON album.AlbumId = track.AlbumId
+JOIN invoice_items invoice_item ON track.TrackId = invoice_item.TrackID
+GROUP BY album.AlbumId
+ORDER BY sold DESC
+LIMIT 3;
