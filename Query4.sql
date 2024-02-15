afdfeb477ebc617b93dbde50849fc18eefc58458
Zachary <zacharyh728@gmail.com>
2024-02-15 00:37:59 -0800

    Wrote and Ran Query4.sql

---
+++ Query4.sql
@@ -0,0 +1,7 @@
+SELECT DISTINCT customer.CustomerId
+FROM customers customer
+INNER JOIN invoices invoice ON customer.CustomerId = invoice.CustomerId
+INNER JOIN invoice_items invoice_item ON invoice.InvoiceId = invoice_item.InvoiceId
+INNER JOIN tracks track ON invoice_item.TrackId = track.TrackId
+WHERE track.Milliseconds >
+    (SELECT AVG(Milliseconds) FROM tracks WHERE Milliseconds < 900000)
